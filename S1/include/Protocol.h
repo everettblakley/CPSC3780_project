c0c8bfb8e6982476e727ff28c1bab58ff9217428
Mitchell Sulz-Martin <sulz@36daredevil.cs.uleth.ca>
2018-12-02 13:41:51 -0700

    Think S1 is done

--- S1/include/Protocol.h
+++ S1/include/Protocol.h
@@ -5,54 +5,64 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <stdio.h>
 
 struct frame{
+  std::string seq;
    std::string parity;
-   std::string data;
-   std::string seq;
+   std::string type;
    std::string line;
+   std::string data;
 };
-
-frame assemble_frame(std::string s) {
-  std::cout << "Assembling frame from: " << s << std::endl;
-  frame outFrame;
-  outFrame.seq = s[0];
-  outFrame.parity = s[1];
-  outFrame.line = s.substr(2, 5);
-  if (s.length() > 7) {
-    outFrame.data = s.substr(7);
-  } else outFrame.data = "garbage";
-  std::cout << outFrame.data << std::endl;
-  return outFrame;
-}
 
 std::string frame_to_string(frame f) {
   std::string outString;
-  outString = f.seq + f.parity + f.line + f.data;
-  std::cout << "Converted frame to " << outString << std::endl;
+  outString = f.seq + f.parity + f.type + f.line + f.data;
+  //std::cout << "Converted frame to " << outString << std::endl;
   return outString;
 }
 
-std::string calcEvenParity(std::string buffer, bool invalid){
-  std::cout << "Calculating Parity Check for : '" << buffer << "'" << std::endl;
-   int sum = 0;
-   for(int i = 0; i < buffer.length(); i++) sum += '0'-buffer[i];
-   std::string pbit = (sum%2==0) ? "0" : "1";
-   return (!invalid) ? pbit : (pbit == "0") ? "1" : "0";
+frame assemble_frame(std::string s) {
+  //std::cout << "Assembling frame from: " << s << std::endl;
+  frame outFrame;
+  outFrame.seq = s[0];
+  outFrame.parity = s[1];
+  outFrame.type = s.substr(2,3);
+  outFrame.line = s.substr(5, 5);
+  if (s.length() > 10) {
+    outFrame.data = s.substr(10);
+  } else outFrame.data = "";
+  //std::cout << frame_to_string(outFrame) << std::endl;
+  return outFrame;
 }
 
-void send_data(std::string frame_no, std::string buffer, ClientSocket& sock) {
+
+
+std::string calcEvenParity(std::string buffer, bool invalid){
+   //std::cout << "Calculating Parity Check for : '" << buffer << "'";
+   int sum = 0;
+   for(int i = 0; i < buffer.length()+1; i++) sum += '0'-buffer[i];
+   std::string pbit = (sum%2==0) ? "0" : "1";
+   //std::cout << " " << pbit << " ";
+   pbit = (!invalid) ? pbit : (pbit == "0") ? "1" : "0";
+   //std::cout << " " << pbit << std::endl;
+   return pbit;
+}
+
+void send_data(std::string frame_no, std::string line_no, std::string buffer, ClientSocket& sock, std::string t) {
   frame s;
-  s.parity = calcEvenParity(buffer, false);
   s.seq = frame_no;
+  s.parity = calcEvenParity(buffer, false);
+  s.type = t;
+  s.line = line_no;
   s.data = buffer;
-  s.line = "00000";
+  // std::cout << frame_to_string(s) << std::endl;
+
   sock << frame_to_string(s);
-  std::cout << "Frame sent to server" << std::endl;
 }
 
 std::vector<frame> buildFrames(){
-   std::ifstream asciiText("file_1.txt");//, std::ios::in|std::ios::binary);
+   std::ifstream asciiText("tutorial.txt");//, std::ios::in|std::ios::binary);
    std::vector<frame> frames;
    std::string line;
    std::string buffer;
@@ -71,7 +81,7 @@ std::vector<frame> buildFrames(){
               if (currentLineLength < 64){
                   // buffer = new char[currentLineLength];
                   bufferLength = currentLineLength;
-                  std::cout<<bufferLength<<std::endl;
+                  //std::cout<<bufferLength<<std::endl;
               }
               else{
                   // buffer = new char[64];
@@ -83,12 +93,13 @@ std::vector<frame> buildFrames(){
               buffer += line.at(j);
           line.erase(0, bufferLength);
           frames[ctr].data = buffer;
-          // invalidFrame = (ctr % 5 == 0) ? true:false;
-          std::cout <<"In buffer before parity check in build frame: " <<buffer << std::endl;
+          invalidFrame = (ctr % 5 == 0) ? true:false;
+          //std::cout <<"In buffer before parity check in build frame: " <<buffer << std::endl;
 
           frames[ctr].parity = calcEvenParity(buffer, invalidFrame);
           frames[ctr].line = lineNum;
           frames[ctr].seq = std::to_string(ctr % 2);
+          frames[ctr].type = "dat";
           ++ctr;
           buffer = "";
       }
@@ -102,48 +113,83 @@ std::vector<frame> buildFrames(){
 }
 
 void protocol_client(ClientSocket& socket) {
-  std::string receivedData;
+  std::string receivedData = "";
   int seq_exp = 0; //When starting a session, initial seq_exp is 0
   frame f; // assemble intial 'request' frame
+  std::string frameType;
+  std::string currentLine = "00001";
+  std::ofstream outputFile ("output.txt", std::ios::out);
   f.seq = std::to_string(seq_exp);
-  send_data(f.seq, "", socket); // send the request to the server
+  frameType = ((f.seq == std::to_string(seq_exp))&&(f.parity == calcEvenParity(f.data, false))) ? "ack" : "nak";
+  //std::cout << "First frame: " << frameType << " " << frame_to_string(f) << std::endl;
+  send_data(f.seq, currentLine, "", socket, frameType); // send the request to the server
   while(true) {
     std::string res;
     socket >> res; // get response from the server
-    std::cout << "Response: " << res << std::endl;
+    // std::cout << "Response: " << res << std::endl;
     f = assemble_frame(res); // parse string into a frame
     std::string parity = calcEvenParity(f.data, false);
     if (f.seq == std::to_string(seq_exp)) {
       if (f.parity == parity) {
-        std::cout << "Frame Parity: " << f.parity << "; Parity check: " << parity << std::endl;
-        receivedData += f.data; // add the res.data into the string of correct data
+                    if (f.line == currentLine) {
+                      receivedData += f.data; // If line is still the current line, add
+                    } else {
+                      if (outputFile.is_open()) {
+                        outputFile << currentLine << " " << receivedData << std::endl;
+                      }
+                      std::cout << currentLine << " " << receivedData << std::endl;
+                      currentLine = f.line;
+                      receivedData = f.data; // If line is still the current line, add
+                    }
+                    //receivedData = f.data; // If line is still the current line, add
+        frameType = "ack";
         seq_exp = (seq_exp + 1) % 2; // if the response seq no matches, increment
       }
+      else{
+        frameType = "nak";
+      }
     }
-    send_data(std::to_string(seq_exp), "", socket); // send the ack/nak frame
+    else{
+      frameType = "nak";
+    }
+    send_data(std::to_string(seq_exp), currentLine, "", socket, frameType); // send the ack/nak frame
   }
-  std::cout << receivedData << std::endl; // output all the received data
+  std::cout << currentLine << " " << receivedData << std::endl;
+  outputFile.close();
 }
 
 void protocol_server(ServerSocket& socket) {
   std::vector<frame> frames = buildFrames(); // all frames for the file
   int frameCtr = 0; // current frame being sent
   int seq_exp = 0; // expected ack frame< std::endl;
-  std::cout << frames.size() << std::endl;
-  while(frameCtr < frames.size()){
-    std::string req;
-    socket >> req; // get request from the socket
+  std::string req;
+  bool first = true;
+  socket >> req;
+  socket << frame_to_string(frames[frameCtr]);
+  while(frameCtr < frames.size()-1){
+    socket >> req;
+    std::cout << "REQUEST: " << req << std::endl;
     frame f = assemble_frame(req); // build a frame from the request
-    if (f.seq == std::to_string(seq_exp)) {
-      socket << frame_to_string(frames[frameCtr]);
+    std::cout << f.type << " " << frame_to_string(f) << " " << frame_to_string(frames[frameCtr])<< std::endl;
+    if (f.type == "ack"){
       seq_exp = (seq_exp + 1) % 2;
       frameCtr+=1;
-      std::cout << "Frame sent to client: " << frameCtr << std::to_string(seq_exp) << std::endl;
-    } else {
-      // f.parity = calcEven  Parity(f.data, false);
-      std::cout << "Frame sent to client: " << frameCtr << std::to_string(seq_exp) << std::endl;
+      std::cout << "ack on frame: " << frame_to_string(frames[frameCtr]) << std::endl;
       socket << frame_to_string(frames[frameCtr]);
     }
+    else{
+        std::cout << "NAK Frames: " << frame_to_string(f) << " : " << frame_to_string(frames[frameCtr]) << std::endl;
+        if(frames[frameCtr].parity != calcEvenParity(frames[frameCtr].data, false)){
+          frames[frameCtr].parity = calcEvenParity(frames[frameCtr].data, false);
+          std::cout << "NAK Frames (After parity change): "  << frame_to_string(frames[frameCtr]) << std::endl;
+        }
+        if(frames[frameCtr].seq != f.seq){
+          std::cout << "NAK seq bad " << frames[frameCtr].seq << " " << std::to_string(seq_exp) << " " << f.seq << std::endl;
+          seq_exp = (seq_exp + 1) % 2;
+          std::cout << "NAK seq bad (after change) " << frames[frameCtr].seq << " " << std::to_string(seq_exp) << " " <<f.seq << std::endl;
+        }
+        socket << frame_to_string(frames[frameCtr]);
+    }
+
   }
-  frames.clear();
 }
