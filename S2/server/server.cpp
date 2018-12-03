7c339c08b667addd39259440dfe3e18bb9845b4a
Dallan (Dallan) McCarthy <mccd3780@36deadpool.cs.uleth.ca>
2018-12-02 17:05:25 -0700

    multiThread the fuck da shit fuck

--- S2/server/server.cpp
+++ S2/server/server.cpp
@@ -2,28 +2,67 @@
 #include "ServerSocket.h"
 #include "SocketException.h"
 #include <string>
+#include <cstdlib>
+#include <iostream>
+#include <pthread.h>
 
-int main(int argc, int argv[])
+#define NUM_THREADS 2
+
+ServerSocket new_sock;
+
+void *Bserver( void *threadid) {
+   long ThreadID;
+   ThreadID = (long)threadid;
+   while(true){
+   try{
+      protocol_server(new_sock);
+      std::cout << "File Transfered" << std::endl;
+   }
+   catch(SocketException& e){
+      std::cout << "Exception caught: " << e.description() << std::endl;
+   }
+   pthread_exit(threadid);
+   }
+}
+
+
+int main()
 {
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   int i=0;
+
    std::cout << "running....\n\n";
+   /*for(i = 0; i < NUM_THREADS; i++) {
+      std:: cout << "Create thread " << i << std::endl;
+      rc = pthread_create( &threads[i], NULL, Bserver, (void *)i);
+
+      if(rc) {
+	 std::cout << "Didnt create thread" << std::endl;
+	 exit(-1);
+	 }*/
+
    try{
       // Create the socket
-      ServerSocket server(30000);
-
-      while (true){
-      	 ServerSocket new_sock;
+      //ServerSocket server(30000);
+      
+      //while (true){
+	 ServerSocket server(30000);
       	 server.accept(new_sock);
-      	 try{
-           protocol_server(new_sock);
-           std::cout << "File Transfered" << std::endl;
-      	 }
-      	 catch(SocketException& e){
-           std::cout << "Exception caught: " << e.description() << std::endl;
-      	 }
+	 for(i = 0; i < NUM_THREADS; i++) {
+	    std:: cout << "Create thread " << i << std::endl;
+	    rc = pthread_create( &threads[i], NULL, Bserver, (void *)i);
+	 }
+	 //std::cout<<"WHat the fuck is rc= " << rc << std::endl;
+	 //rc = pthread_create( &threads[i], NULL, Bserver, (void *)i);
+	 //std::cout<<"WHat the fuck is rc= " << rc << std::endl;
+	 //pthread_exit(NULL);
       }
-   }
+   // }
    catch (SocketException& e){
       std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+      pthread_exit(NULL);
    }
   return 0;
 }
+
