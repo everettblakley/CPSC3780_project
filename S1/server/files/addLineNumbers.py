5eb62dfcd83a561f34cd4e5538548bcd56782bf6
Mitchell Sulz-Martin <sulz@36daredevil.cs.uleth.ca>
2018-12-03 14:54:01 -0700

    Added large files

--- S1/server/files/addLineNumbers.py
+++ S1/server/files/addLineNumbers.py
@@ -1,26 +1,29 @@
 import os
 
-# for R,D,F in os.walk(os.getcwd()):
-#     for f in F:
-#         if f.endswith(".txt"):
-#             inTxt = open(os.path.join(R, f), "r").readlines()
-#             outFileName = f[:f.rfind(".")]  + "_fixed.txt"
-#             outTxt = open(os.path.join(R, outFileName), "w")
-#             for line in inTxt:
-#                 outTxt.write('0{0}'.format(line))
-#
-#             print "Completed " + f
-#             outTxt.close()
-#
-# for R,D,F in os.walk(os.getcwd()):
-#     for f in F:
-#         if f.endswith(".txt"):
-inTxt = open("sherlock_holmes.txt", "r").readlines()
-outFileName = "sherlock_holmes_fixed.txt"
-outTxt = open(outFileName, "w")
-for line in inTxt:
-    lineNum = int(line[:line.find(" ")])
-    outTxt.write('{0:0>6} {1}'.format(lineNum, line[line.find(" "):]))
+for R,D,F in os.walk(os.getcwd()):
+    for f in F:
+        # if f.endswith(".txt"):
+        if (f == "TCP.txt"):
+            inTxt = open(os.path.join(R, f), "r").readlines()
+            outFileName = f[:f.rfind(".")]  + "_fixed.txt"
+            outTxt = open(os.path.join(R, outFileName), "w")
+            for line in inTxt:
+                outTxt.write('0{0}'.format(line))
 
-print "Completed "
-outTxt.close()
+            print "Completed " + f
+            outTxt.close()
+
+# for R,D,F in os.walk(os.getcwd()):
+#     for f in F:
+#         if f.endswith(".txt"):
+
+# inTxt = open("TCP.txt", "r").readlines()
+# outFileName = "sherlock_holmes_fixed.txt"
+# outTxt = open(outFileName, "w")
+# for line in inTxt:
+#     lineNum = int(line[:line.find(" ")])
+#     print(lineNum)
+#     outTxt.write('{0:0>6} {1}'.format(lineNum, line[line.find(" "):]))
+#
+# print "Completed "
+# outTxt.close()
